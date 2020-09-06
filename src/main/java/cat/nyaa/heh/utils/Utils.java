eea000a2e62f5bdf1800875a89f3104013dd19c2
ReinWD <ReinWDD@gmail.com>
2020-09-06 19:50:19 +0800

    add button in Utils

--- src/main/java/cat/nyaa/heh/utils/Utils.java
+++ src/main/java/cat/nyaa/heh/utils/Utils.java
@@ -1,9 +1,15 @@
 package cat.nyaa.heh.utils;
 
 import cat.nyaa.heh.HamsterEcoHelper;
+import cat.nyaa.heh.I18n;
 import co.aikar.taskchain.BukkitTaskChainFactory;
 import co.aikar.taskchain.TaskChain;
 import co.aikar.taskchain.TaskChainFactory;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.chat.hover.content.Text;
 import org.apache.commons.lang.text.StrSubstitutor;
 import org.bukkit.ChatColor;
 
@@ -36,5 +42,12 @@ public class Utils {
         int size = collection.size();
         int rnd = ThreadLocalRandom.current().nextInt(size);
         return collection.stream().skip(rnd).findFirst().orElse(null);
+    }
+
+    public TextComponent newMessageButton(String buttonText, HoverEvent hoverEvent, ClickEvent clickEvent){
+        TextComponent button = new TextComponent(buttonText);
+        button.setHoverEvent(hoverEvent);
+        button.setClickEvent(clickEvent);
+        return button;
     }
 }
