ffbb9fb3baf1af71cf5c2467edd32cb3e4367b9f
ReinWD <ReinWDD@gmail.com>
2020-10-19 13:48:51 +0800

    NPE

--- src/main/java/cat/nyaa/heh/business/item/ModelableItem.java
+++ src/main/java/cat/nyaa/heh/business/item/ModelableItem.java
@@ -3,6 +3,7 @@ package cat.nyaa.heh.business.item;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -19,6 +20,9 @@ public interface ModelableItem<T> {
         }
         itemMeta = originMeta.clone();
         List<String> lore = itemMeta.getLore();
+        if (lore == null){
+            lore = new ArrayList<>();
+        }
         Collection<? extends String> lore1 = buildLore();
         lore.addAll(lore1);
         itemMeta.setLore(lore);
